52381ecf257bd4d80c318156da16185b7b5e490b
Glitch (rb-styled-playlist) <none>
2019-02-26 14:27:44 +0000

    ⛽️🚇 Updated with Glitch

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -15,16 +15,14 @@ const playlist = {
   },
 
   deleteSong(request, response) {
-
     const playlistId = request.params.id;
     const songId = request.params.songid;
     logger.debug(`Deleting Song ${songId} from Playlist ${playlistId}`);
     playlistStore.removeSong(playlistId, songId);
     response.redirect('/playlist/' + playlistId);
-    
   },
   
-    deletePlayList(request, response) {
+  deletePlayList(request, response) {
     const playlistId = request.params.id;
     logger.debug(`Deleting Playlist ${playlistId}`);
     playlistStore.removePlaylist(playlistId);
